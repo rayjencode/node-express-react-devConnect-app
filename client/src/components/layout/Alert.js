fde8cae4ee409fc5fea7de802a9125c62013ce57
rayjenscode <rayjenscode@gmail.com>
2019-06-29 15:17:18 +0800

    added type

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Alert = ({ alerts }) => {
+  return <div />;
+};
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired
+};
+const mapStateToProps = state => ({
+  alerts: state.alert
+});
+export default connect()(Alert);
